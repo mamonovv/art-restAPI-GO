985bbba89b0c50d1dd49a94ef1e528772a99dbdd
mamonov <mmvvlad@gmail.com>
2022-08-16 16:40:04 +0700

    initial commit

---
+++ cmd/main/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"art-rest/internal/user"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func main() {
+	log.Println("create Router")
+	router := httprouter.New()
+
+	log.Println("register user handler")
+	handler := user.NewHandler()
+	handler.Register(router)
+
+	start(router)
+}
+
+func start(router *httprouter.Router) {
+	log.Println("start application")
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("server is listening port 0.0.0.0:1234")
+	log.Fatal(server.Serve(listener))
+}
---
+++ internal/user/handler.go
@@ -0,0 +1,58 @@
+package user
+
+import (
+	"art-rest/internal/handlers"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+const (
+	usersUrl = "/users"
+	userUrl  = "/users/:uid"
+)
+
+type handler struct {
+}
+
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersUrl, h.GetList)
+	router.GET(userUrl, h.GetUserByUUID)
+	router.POST(userUrl, h.CreateUser)
+	router.PUT(userUrl, h.UpdateUser)
+	router.PATCH(userUrl, h.PartiallyUpdateUser)
+	router.DELETE(userUrl, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("This is list of users!"))
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("This get user by uid!"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("This is create user!"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("This is update user!"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("This is partially update user!"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("This is delete user!"))
+}
